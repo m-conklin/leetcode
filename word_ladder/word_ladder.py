0dfe65775507d3a152c30bdeb72e1bbcd4a69542
Martin Conklin <mjmc@uvic.ca>
2015-01-14 19:27:06 +0000

    finds list of start and end nodes

--- word_ladder/word_ladder.py
+++ word_ladder/word_ladder.py
@@ -3,5 +3,32 @@
 start = 'hit'
 end = 'cog'
 dict = ['hot','dot','dog','lot','log']
+word_length = len(start)
 
-print start, end, dict
+start_list =[]
+letter_match = 0 
+
+for word in dict:
+    for i in range(len(word)):
+        if word[i] ==  start[i]:
+            letter_match += 1
+    if letter_match == len(word)- 1:
+        start_list.append(word)
+    letter_match = 0
+
+print 'start_list:',start_list
+
+end_list =[]
+letter_match = 0
+
+for word in dict:
+    for i in range(len(word)):
+        if word[i] ==  end[i]:
+            letter_match += 1
+    if letter_match == len(word)- 1:
+        end_list.append(word)
+    letter_match = 0
+print 'end_list:', end_list
+
+
+
