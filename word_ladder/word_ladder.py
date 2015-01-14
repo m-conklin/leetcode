cb5029ba8846cbffa79b5bc2c3bc9e2c95bdbff9
Martin Conklin <mjmc@uvic.ca>
2015-01-14 23:47:30 +0000

    represents dict as a graph in 2D list, upper right

--- word_ladder/word_ladder.py
+++ word_ladder/word_ladder.py
@@ -3,16 +3,20 @@
 start = 'hit'
 end = 'cog'
 dict = ['hot','dot','dog','lot','log']
-word_length = len(start)
+wl = len(start)
+w_check = wl - 1
+dl = len(dict)
+
+print 'dict:', dict
 
 start_list =[]
 letter_match = 0 
 
 for word in dict:
-    for i in range(len(word)):
+    for i in range(wl):
         if word[i] ==  start[i]:
             letter_match += 1
-    if letter_match == len(word)- 1:
+    if letter_match == w_check:
         start_list.append(word)
     letter_match = 0
 
@@ -22,19 +26,36 @@ end_list =[]
 letter_match = 0
 
 for word in dict:
-    for i in range(len(word)):
+    for i in range(wl):
         if word[i] ==  end[i]:
             letter_match += 1
-    if letter_match == len(word)- 1:
+    if letter_match == w_check:
         end_list.append(word)
     letter_match = 0
 print 'end_list:', end_list
 
 
-node = [0 for i in dict]
-graph = [node for i in dict]
+graph = [[0 for i in dict] for i in dict]
 
    
 print 'Graph:'
-for node in graph:
-    print node
+for v in graph:
+    print v
+
+letter_match = 0
+
+for i in range(dl-1):
+    for j in range((i+1),dl):
+        word = dict[i]
+        comp = dict[j]
+        for k in range(wl):
+            if word[k] == comp[k]:
+                letter_match += 1
+        if letter_match == w_check:
+            graph[i][j] = 1
+        letter_match = 0
+
+print 'Graph:'
+for v in graph:
+    print v
+
