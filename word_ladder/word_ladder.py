b4caa1c2c6db2222de98c01c458f228e596fbadb
Martin Conklin <mjmc@uvic.ca>
2015-01-15 01:25:54 +0000

    shortest path in vertices 2D list

--- word_ladder/word_ladder.py
+++ word_ladder/word_ladder.py
@@ -6,34 +6,37 @@ dict = ['hot','dot','dog','lot','log']
 wl = len(start)
 w_check = wl - 1
 dl = len(dict)
-
+r_dl = range(dl)
 print 'dict:', dict
 
 start_list =[]
 letter_match = 0 
 
-for word in dict:
+for word in r_dl:
     for i in range(wl):
-        if word[i] ==  start[i]:
+        if dict[word][i] ==  start[i]:
             letter_match += 1
     if letter_match == w_check:
         start_list.append(word)
     letter_match = 0
 
 print 'start_list:',start_list
+for i in start_list:
+    print dict[i]
 
 end_list =[]
 letter_match = 0
 
-for word in dict:
+for word in r_dl:
     for i in range(wl):
-        if word[i] ==  end[i]:
+        if dict[word][i] ==  end[i]:
             letter_match += 1
     if letter_match == w_check:
         end_list.append(word)
     letter_match = 0
 print 'end_list:', end_list
-
+for i in end_list:
+    print dict[i]
 
 graph = [[0 for i in dict] for i in dict]
 
@@ -52,10 +55,31 @@ for i in range(dl-1):
             if word[k] == comp[k]:
                 letter_match += 1
         if letter_match == w_check:
-            graph[i][j] = 1
+            graph[i][j],graph[j][i] = 1,1
         letter_match = 0
 
 print 'Graph:'
 for v in graph:
     print v
 
+vertices = [[None,None] for word in dict]
+
+queue = []
+
+for start_vertex in start_list:
+#    for end_vertex in end_list:
+        queue.append(start_vertex)
+        vertices[start_vertex][0] = 0
+
+        while len(queue) > 0:
+            dq = queue.pop(0)
+            for adj in r_dl:
+                if graph[dq][adj] == 1 and vertices[adj][0] == None:
+                    queue.append(adj)
+                    dist = vertices[dq][0] + 1
+                    vertices[adj] = [dist,dq]
+
+#        print 'end:',end_vertex, dict[end_vertex]
+        print 'vert:',vertices
+
+            
